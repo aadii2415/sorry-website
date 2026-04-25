a086f903c9c0a869a2a44e631bd1f95bce9b99fb
Aaditya <adityapsinghs98@gmail.com>
2026-04-26 01:38:47 +0530

    Added music feature 🎵

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-// Save her reply
+// Save reply (non-blocking)
 app.post("/reply", (req, res) => {
     const message = req.body.message;
 
-    fs.appendFileSync("replies.txt", message + "\n");
+    fs.appendFile("replies.txt", message + "\n", (err) => {
+        if (err) {
+            return res.status(500).json({ status: "Error saving message" });
+        }
+        res.json({ status: "Your message reached my heart ❤️" });
+    });
+});
 
-    res.json({ status: "Your message reached my heart ❤️" });
+// Get all replies
+app.get("/messages", (req, res) => {
+    fs.readFile("replies.txt", "utf-8", (err, data) => {
+        if (err) {
+            return res.send("No messages yet 💌");
+        }
+        res.send(data);
+    });
 });
 
 app.listen(3000, () => {
